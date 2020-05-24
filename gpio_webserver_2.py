beac2279b8ef5660dbded21041f3978f0158719e
gtra389 <52122223+gtra389@users.noreply.github.com>
2020-05-24 23:24:41 +0800

    Add files via upload

--- gpio_webserver_2.py
+++ gpio_webserver_2.py
@@ -16,6 +16,8 @@ import BaseHTTPServer
 from BaseHTTPServer import BaseHTTPRequestHandler
 import SocketServer
 from threading import Condition
+import io
+import picamera
 
 
 host_name = '0.0.0.0'    # Change this to your Raspberry Pi IP address
@@ -25,12 +27,33 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 # Create a dictionary called pins to store the pin number, name, and pin state:
-pins = {
-   26 : {'name' : 'GPIO 26', 'state' : GPIO.HIGH},
-   20 : {'name' : 'GPIO 20', 'state' : GPIO.HIGH},
-   21 : {'name' : 'GPIO 21', 'state' : GPIO.HIGH}
-   }
+pins = [26, 20, 21]
 
+# Set each pin as an output and make it low:
+for pin in pins:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.LOW)
+print("Initiation is done.")
+print("")
+
+
+# Configuration of Pi camera
+class StreamingOutput(object):
+    def __init__(self):
+        self.frame = None
+        self.buffer = io.BytesIO()
+        self.condition = Condition()
+
+    def write(self, buf):
+        if buf.startswith(b'\xff\xd8'):
+            # New frame, copy the existing buffer's content and notify all
+            # clients it's available
+            self.buffer.truncate()
+            with self.condition:
+                self.frame = self.buffer.getvalue()
+                self.condition.notify_all()
+            self.buffer.seek(0)
+        return self.buffer.write(buf) 
 
 
 class MyServerHandler(BaseHTTPRequestHandler):
@@ -70,6 +93,10 @@ class MyServerHandler(BaseHTTPRequestHandler):
             <script>
                 document.getElementById("current-status").innerHTML="{}";
             </script>
+
+            <h1>PiCamera MJPEG Streaming Demo</h1>
+            <img src="stream.mjpg" width="640" height="480" />
+
             </body>
             </html>
         '''
@@ -77,13 +104,9 @@ class MyServerHandler(BaseHTTPRequestHandler):
         self.do_HEAD()
         status = ''
         if self.path=='/':
-            # Set each pin as an output and make it low:
-            for pin in pins:
-                GPIO.setup(pin, GPIO.OUT)
-                GPIO.output(pin, GPIO.LOW)
-            print("Initiation is done.")
-            print("")
-
+            self.send_response(301)
+            self.send_header('Location', '/index.html')
+            self.end_headers()
         elif self.path=='/ch1/on':
             GPIO.output(26, GPIO.HIGH)
             status='Relay Channel 1 is On'
@@ -96,12 +119,44 @@ class MyServerHandler(BaseHTTPRequestHandler):
         elif self.path=='/ch2/off':
             GPIO.output(20, GPIO.LOW)
             status='Relay Channel 2 is Off'
-        elif self.path=='/ch2/on':
+        elif self.path=='/ch3/on':
             GPIO.output(21, GPIO.HIGH)
             status='Relay Channel 3 is On'
         elif self.path=='/ch3/off':
             GPIO.output(21, GPIO.LOW)
             status='Relay Channel 3 is Off'
+                elif self.path == '/index.html':
+                content = PAGE.encode('utf-8')
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(content))
+            self.end_headers()
+            self.wfile.write(content)
+        elif self.path == '/stream.mjpg':
+            self.send_response(200)
+            self.send_header('Age', 0)
+            self.send_header('Cache-Control', 'no-cache, private')
+            self.send_header('Pragma', 'no-cache')
+            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+            self.end_headers()
+            try:
+                while True:
+                    with output.condition:
+                        output.condition.wait()
+                        frame = output.frame
+                    self.wfile.write(b'--FRAME\r\n')
+                    self.send_header('Content-Type', 'image/jpeg')
+                    self.send_header('Content-Length', len(frame))
+                    self.end_headers()
+                    self.wfile.write(frame)
+                    self.wfile.write(b'\r\n')
+            except Exception as e:
+                logging.warning(
+                    'Removed streaming client %s: %s',
+                    self.client_address, str(e))
+        else:
+            self.send_error(404)
+            self.end_headers()        
 
         self.wfile.write(html.format(temp[5:], status).encode("utf-8"))
 
@@ -111,10 +166,12 @@ class StreamingServer(SocketServer.ThreadingMixIn, BaseHTTPServer.HTTPServer):
 
         
 if __name__ == '__main__':
+    with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
+        output = StreamingOutput()
+        camera.start_recording(output, format='mjpeg')
 	address = (host_name, host_port)
 	http_server = StreamingServer(address, MyServerHandler)
 	print("Server Starts - %s:%s" % (host_name, host_port))
-
 	try:
 		http_server.serve_forever()
 	except KeyboardInterrupt:
