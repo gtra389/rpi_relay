4a2b7bf4fbc36e0c0221d21bf046a945dea6b1fb
gtra389 <52122223+gtra389@users.noreply.github.com>
2020-05-24 22:26:36 +0800

    Add files via upload

--- gpio_webserver_2.py
+++ gpio_webserver_2.py
@@ -1,3 +1,13 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+##################################################
+
+#       P26 ----> Relay_Ch1
+#	    P20 ----> Relay_Ch2
+#	    P21 ----> Relay_Ch3
+
+##################################################
+
 import RPi.GPIO as GPIO
 import os
 #from http.server import BaseHTTPRequestHandler, HTTPServer
@@ -10,6 +20,17 @@ from threading import Condition
 
 host_name = '0.0.0.0'    # Change this to your Raspberry Pi IP address
 host_port = 8080
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+# Create a dictionary called pins to store the pin number, name, and pin state:
+pins = {
+   26 : {'name' : 'GPIO 26', 'state' : GPIO.HIGH},
+   20 : {'name' : 'GPIO 20', 'state' : GPIO.HIGH},
+   21 : {'name' : 'GPIO 21', 'state' : GPIO.HIGH}
+   }
+
 
 
 class MyServerHandler(BaseHTTPRequestHandler):
@@ -30,14 +51,24 @@ class MyServerHandler(BaseHTTPRequestHandler):
             'curl http://server-ip-address:port' 
         """
         html = '''
-            <html>
+            <head>
+            <title>RPi Web Server</title>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+            <!-- Optional theme -->
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+            <!-- Latest compiled and minified JavaScript -->
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+            </head>
+
             <body style="width:960px; margin: 20px auto;">
-            <h1>Welcome to my Raspberry Pi</h1>
             <p>Current GPU temperature is {}</p>
-            <p>Turn LED: <a href="/on">On</a> <a href="/off">Off</a></p>
-            <div id="led-status"></div>
+            <p>Turn Relay Channel 1: <a href="/ch1/on">ON</a> <a href="/ch1/off">OFF</a></p>
+            <p>Turn Relay Channel 2: <a href="/ch2/on">ON</a> <a href="/ch2/off">OFF</a></p>
+            <p>Turn Relay Channel 3: <a href="/ch3/on">ON</a> <a href="/ch3/off">OFF</a></p>
+            <p>Current status</p>
+            <div id="current-status"></div>
             <script>
-                document.getElementById("led-status").innerHTML="{}";
+                document.getElementById("current-status").innerHTML="{}";
             </script>
             </body>
             </html>
@@ -46,15 +77,32 @@ class MyServerHandler(BaseHTTPRequestHandler):
         self.do_HEAD()
         status = ''
         if self.path=='/':
-            GPIO.setmode(GPIO.BCM)
-            GPIO.setwarnings(False)
-            GPIO.setup(26, GPIO.OUT)
-        elif self.path=='/on':
+            # Set each pin as an output and make it low:
+            for pin in pins:
+                GPIO.setup(pin, GPIO.OUT)
+                GPIO.output(pin, GPIO.LOW)
+            print("Initiation is done.")
+            print("")
+
+        elif self.path=='/ch1/on':
             GPIO.output(26, GPIO.HIGH)
-            status='LED is On'
-        elif self.path=='/off':
+            status='Relay Channel 1 is On'
+        elif self.path=='/ch1/off':
             GPIO.output(26, GPIO.LOW)
-            status='LED is Off'
+            status='Relay Channel 1 is Off'
+        elif self.path=='/ch2/on':
+            GPIO.output(20, GPIO.HIGH)
+            status='Relay Channel 2 is On'
+        elif self.path=='/ch2/off':
+            GPIO.output(20, GPIO.LOW)
+            status='Relay Channel 2 is Off'
+        elif self.path=='/ch2/on':
+            GPIO.output(21, GPIO.HIGH)
+            status='Relay Channel 3 is On'
+        elif self.path=='/ch3/off':
+            GPIO.output(21, GPIO.LOW)
+            status='Relay Channel 3 is Off'
+
         self.wfile.write(html.format(temp[5:], status).encode("utf-8"))
 
 class StreamingServer(SocketServer.ThreadingMixIn, BaseHTTPServer.HTTPServer):
